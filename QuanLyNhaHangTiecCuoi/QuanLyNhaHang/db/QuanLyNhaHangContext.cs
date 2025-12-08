60abd9410069168cb100da311a5fec62d16c4796
vyvankhien <khienvvpk04186@gmail.com>
2025-12-08 12:47:24 +0700

    sửa đường dẫn

--- QuanLyNhaHangTiecCuoi/QuanLyNhaHang/db/QuanLyNhaHangContext.cs
+++ QuanLyNhaHangTiecCuoi/QuanLyNhaHang/db/QuanLyNhaHangContext.cs
@@ -17,7 +17,7 @@ namespace QuanLyNhaHang.Models
             {
                 // Sửa lại cho khớp với máy tính của bạn (SQLEXPRESS)
                 // Lưu ý: Trong C# string dùng @ đằng trước thì 
-                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-NVBKKGJ\SQLEXPRESS;Initial Catalog=QL_NhaHangTiecCuoiLongPhung;Integrated Security=True;Trust Server Certificate=True");
+                optionsBuilder.UseSqlServer(@"Data Source=YKHIENBLO\SQLEXPRESS01;Initial Catalog=QL_NhaHangTiecCuoiLongPhung;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
 
             }
         }
